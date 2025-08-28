3ab67938e4432a1d535fc34ea257fc2403aabf3f
Sadman Sakib <s.sakib01316448433@gmail.com>
2025-08-28 12:08:23 +0600

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Login Page");
-        calculator c1 = new calculator();
-        c1.calculator();
-
-    }
-}
